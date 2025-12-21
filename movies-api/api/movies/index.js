51c7594a9f28738370e04f4bd13dee35be178516
Jer Power <147807820+Jer-Power1@users.noreply.github.com>
2025-12-21 18:38:33 +0000

    Expanded movies router using TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,12 +1,55 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import {getMovies, getMovie, getUpcomingMovies, getGenres, getTopRatedMovies, getTrendingThisWeek, getMovieCredits, getPerson} from '../tmdb-api'; 
 
 const router = express.Router();
 
+// Discover movies (homepage)
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+// Get upcoming movies
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
+// Get movie genres
+router.get('/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
+}));
+
+// Get top rated movies
+router.get('/top-rated', asyncHandler(async (req, res) => {
+    const topRatedMovies = await getTopRatedMovies();
+    res.status(200).json(topRatedMovies);
+}));
+
+// Get trending movies this week
+router.get('/trending/week', asyncHandler(async (req, res) => {
+    const trendingMovies = await getTrendingThisWeek();
+    res.status(200).json(trendingMovies);
+}));
+
+ // Get movie details by id
+router.get('/:id', asyncHandler(async (req, res) => {
+    const movie = await getMovie(req.params.id);
+    res.status(200).json(movie);
+}));
+
+// Get movie credits (cast & crew)
+router.get('/:id/credits', asyncHandler(async (req, res) => {
+    const credits = await getMovieCredits(req.params.id);
+    res.status(200).json(credits);
+}));
+
+// Get person details
+router.get('/person/:id', asyncHandler(async (req, res) => {
+    const person = await getPerson(req.params.id);
+    res.status(200).json(person);
 }));
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,13 +1,68 @@
 import fetch from 'node-fetch';
 
-export const getMovies = async () => {
-    const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-    );
+const baseUrl = "https://api.themoviedb.org/3";
 
+async function fetchJson(url) {
+    const response = await fetch(url);
     if (!response.ok) {
-        throw new Error(response.json().message);
+        const error = await response.json().catch(() => ({}));
+        throw new Error(error.status_message || "TMDB request failed");
     }
+    return response.json();
+}
 
-    return await response.json();
+// Discover (homepage)
+export const getMovies = async () => {
+    return fetchJson(
+    `${baseUrl}/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  );
+};
+
+// Single movie details
+export const getMovie = async (id) => {
+  return fetchJson(
+    `${baseUrl}/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+};
+
+// Upcoming
+export const getUpcomingMovies = async () => {
+  return fetchJson(
+    `${baseUrl}/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
+};
+
+// Genres
+export const getGenres = async () => {
+  return fetchJson(
+     `${baseUrl}/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+};
+
+// Top rated
+export const getTopRatedMovies = async () => {
+  return fetchJson(
+    `${baseUrl}/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
+};
+
+// Trending (week)
+export const getTrendingThisWeek = async () => {
+  return fetchJson(
+    `${baseUrl}/trending/movie/week?api_key=${process.env.TMDB_KEY}`
+  );
+};
+
+// Credits (cast/crew)
+export const getMovieCredits = async (id) => {
+  return fetchJson(
+    `${baseUrl}/movie/${id}/credits?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+};
+
+// Person details
+export const getPerson = async (id) => {
+  return fetchJson(
+    `${baseUrl}/person/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
 };
